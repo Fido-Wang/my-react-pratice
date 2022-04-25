054651bf328ddde1c1c4cad8cb1eefb260e37131
wangqi <742927821@qq.com>
2022-04-25 22:30:24 +0800

    1212

--- src/component/main/Home/index.jsx
+++ src/component/main/Home/index.jsx
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom'
 import { Button } from 'antd';
+import { Row, Col } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { DownloadOutlined } from '@ant-design/icons';
 import Message from './Pages/Message'
 import News from './Pages/News'
 import MyNavLink from "../../MyNavLink";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 class Home extends Component {
     render() {
         console.log('home组件收到的props', this.props)
         return (
-            <div>
-                <h2>Home</h2>
-                <MyNavLink style={{ padding: '12px'}} to="/home/message">Message</MyNavLink>
-                <MyNavLink style={{ padding: '12px'}} to="/home/news">News</MyNavLink>
+            <div style={{ background: 'green', height: '100%'}}>
+                <Button type="primary" icon={<DownloadOutlined />} size="small">
+                    Download
+                </Button>
+                <Row>
+                    <Col span={24}><h2>Home</h2></Col>
+
+                </Row>
+                <Row>
+                    <MyNavLink style={{ padding: '12px'}} to="/home/message">Message</MyNavLink>
+
+                    <MyNavLink style={{ padding: '12px'}} to="/home/news">News</MyNavLink>
+                </Row>
+                <Row>
+
+                </Row>
+
                 <Outlet/>
             </div>
         );
